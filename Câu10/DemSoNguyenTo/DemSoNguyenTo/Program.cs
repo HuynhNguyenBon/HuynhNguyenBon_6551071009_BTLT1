7338d5a49266b34d03942d1f6adbc5d89aab1db9
Nguyên Bổn <huynhnguyenbon@gmail.com>
2026-08-25 09:49:30 +0700

    Add files via upload

---
+++ Câu10/DemSoNguyenTo/DemSoNguyenTo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DemSoNguyenTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CHUONG TRINH DEM SO NGUYEN TO ===");
+
+            try
+            {
+                Console.Write("Nhap so nguyen duong n (n <= 1000): ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\n--- CAC SO NGUYEN TO TU 2 DEN {n} ---");
+                int demSoLuong = 0;
+
+                for (int i = 2; i <= n; i++)
+                {
+                    bool laSoNguyenTo = true;
+
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            laSoNguyenTo = false;
+                            break;
+                        }
+                    }
+
+                    if (laSoNguyenTo)
+                    {
+                        Console.Write(i + " ");
+                        demSoLuong++;
+                    }
+                }
+
+                Console.WriteLine($"\n\nTong so luong so nguyen to tim duoc: {demSoLuong}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Loi: Vui long nhap mot so nguyen hop le!");
+            }
+        }
+    }
+}
